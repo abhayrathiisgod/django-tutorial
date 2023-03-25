12e23ab1b096aaecf95f0f68630c80fc173818f5
Abhay Rathi <rathiabhay33@gmail.com>
2023-03-25 10:55:02 +0530

    session 2

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns =[
     #root url
     path('',views.index,name ='index'),
-    path('counter',views.counter, name='counter')
+    path('counter',views.counter, name='counter'),
+    path('register',views.register, name='register')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from .models import Feature
 # Create your views here.
 def index(request):
     # cant put all in here ->>>>> return HttpResponse('<h1>I am batman!</h1>')
@@ -9,12 +11,32 @@ def index(request):
     # now we need name from the database so
     #name = user.name
     #good practice is to make dictionary
+    
+    features = Feature.objects.all()
+
     context = {
         'name': 'Abhay Rathi',
         'age':23,
         'nationality':'Nepalese'
     }
-    return render(request,'index.html')   # like dictionary ->>{key:value}
+    return render(request,'index.html',{'features': features})   # like dictionary ->>{key:value}
+
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        #if password ==  password2:
+            #return
+
+
+
+
+    return render(request,'register.html')
+
 
 def counter(request):
     text = request.POST['text']  # text????? ---> from the html form pa
